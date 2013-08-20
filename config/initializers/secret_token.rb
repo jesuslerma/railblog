680d165cdd18f835cfbfcfe801234ffb0f557f92
Jesus Lerma <shuyojl@gmail.com>
2013-08-20 08:10:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubzombie::Application.config.secret_key_base = 'bc45654f0f278de591a9c14e7440194828c325117a6f8750ae3640506e6c05fed259b313ac4df3d11a3df76d6a03895eb7840b55844d65bd2b938e9e34f01873'
